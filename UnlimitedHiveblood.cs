ec824ce867d43d2a2b7c414a3d849cb541dbb80b
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2024-09-17 18:15:19 -0500

    Fixed a bug when opening a save file a second time

--- UnlimitedHiveblood.cs
+++ UnlimitedHiveblood.cs
@@ -7,7 +7,7 @@ using HutongGames.PlayMaker;
 namespace UnlimitedHiveblood {
     public class UnlimitedHiveblood: Mod {
         new public string GetName() => "UnlimitedHiveblood";
-        public override string GetVersion() => "1.0.0.0";
+        public override string GetVersion() => "1.0.0.1";
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) {
             On.PlayMakerFSM.OnEnable += editFSM;
         }
@@ -15,9 +15,7 @@ namespace UnlimitedHiveblood {
         private void editFSM(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self) {
             orig(self);
             if(self.gameObject.name == "Health" && self.FsmName == "Hive Health Regen") {
-                FsmEvent hiveSynergy = new FsmEvent("HIVE SYNERGY");
                 whiteHiveSynergy whiteHiveAction = new();
-                whiteHiveAction.doSynergy = hiveSynergy;
                 FsmState idleState = self.GetState("Idle");
                 idleState.AddAction(whiteHiveAction);
                 idleState.AddTransition("HIVE SYNERGY", "Start Recovery");
@@ -26,11 +24,10 @@ namespace UnlimitedHiveblood {
     }
 
     public class whiteHiveSynergy: FsmStateAction {
-        public FsmEvent doSynergy;
         public override void OnEnter() {
             PlayerData pd = PlayerData.instance;
             if(pd.royalCharmState >= 3 && pd.equippedCharm_36 && pd.equippedCharm_29 && pd.health < pd.maxHealth) {
-                base.Fsm.Event(doSynergy);
+                base.Fsm.Event("HIVE SYNERGY");
             }
         }
     }
